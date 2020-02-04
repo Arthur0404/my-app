eee410d334b69b7aef78bc98317714098cfaa74e
Artur B <predator777799@gmail.com>
2020-02-04 19:08:08 +0300

    new fail

--- src/App.js
+++ src/App.js
@@ -1,35 +1,19 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
 
 const App = () => {
   return (
-      <div>
-          <Header />
-          <Techologies />
+      <div className='app-wrapper'>
+            <Header />
+            <Navbar/>
+            <Profile/>
       </div>
   );
-}
+};
 
-const Techologies = () => {
-   return (
-       <div>
-           <ul>
-               <li>css</li>
-               <li>HTML</li>
-               <li>JS</li>
-               <li>React</li>
-           </ul>
-       </div>
-   )
-}
-const Header = () => {
-    return (<div>
-            <a href="#">Home</a>
-            <a href="#">Page</a>
-            <a href="#">New Feed</a>
-            <a href="#">Messages</a>
-        </div>
-    );
-}
 
 export default App;
---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import React from "react";
+import './Profile.css';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://otdoxnite.ru/wp-content/uploads/2016/11/goafon2.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
